792d64717814396a93aee1c6118b6b9876cae287
Ungigdu <Ungigdu@protonmail.com>
2020-07-15 15:47:08 +0800

    change ap

--- eth/connection.go
+++ eth/connection.go
@@ -1,5 +1,5 @@
 package eth
 
-var AccessPoint = "wss://ropsten.infura.io/ws/v3/8533ef82c9744d38801f512fdd004133"
+var AccessPoint = "wss://ropsten.infura.io/ws/v3/fe5ffffba0bf46bb9dc27fd5e04cd6cb"
 
 var EventNotificationAddress = "0xB5e658F0F77A86c6D61bd3666C784d4A581dc40e"
